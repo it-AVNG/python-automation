c9e8ed9741ad95765709e1b801aa4630591930c1
it-AVNG <it.avng@gmail.com>
2023-09-30 16:11:20 +0700

    refractor scraping loop to single run

--- automate-tasks-study/beautifulSoup/scrape_real.py
+++ automate-tasks-study/beautifulSoup/scrape_real.py
@@ -24,12 +24,10 @@ def find_itviec_django_job():
             f.write(f'Position {str(i+1)}: {position_name}\n Company : {company_name}\n Date post: {date_post}\n link: {link_job}\n ---------------------- \n')
 
 if __name__ == '__main__':
-    # while True:
-        print(f'look up django job on itviec.com: ')
-        now = datetime.now()
-        with open(f'posts/itviec.txt', 'w') as f:
-             f.write(f'Date post: {now} \n')
-        find_itviec_django_job()
-        time_wait= 240
-        print(f'wrote output to file! please check the file')
-        time.sleep(time_wait * 60)
+    print(f'look up django job on itviec.com: ')
+    now = datetime.now()
+    with open(f'posts/itviec.txt', 'w') as f:
+        f.write(f'Date post: {now} \n')
+    find_itviec_django_job()
+    time_wait= 240
+    print(f'wrote output to file! please check the file')
